f68bc0db91a9a3e3ddc142524a69d17bbf13343a
Brook Nega <brooknega21@gmail.com>
2022-09-19 18:19:15 -0400

    commit tree a6acadd5c685e7c11d494aa4f857bc25a7c760b2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = names => [...names].splice(0,2)
+
+const returnLastTwoDrivers = names => [...names].splice(-2)
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return function multiplier(){
+        return num * num
+    }
+}
+
+function fareDoubler(){
+    const doubler = createFareMultiplier(num)
+    return doubler
+}
